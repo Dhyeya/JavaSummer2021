c164f263020e00488772e57030f1efe9a9199180
Dhyeya <dhyeya.chandi@gmail.com>
2021-06-24 10:44:42 -0400

    Completed.

---
+++ 2HelloWorld/src/HW2_1.java
@@ -0,0 +1,11 @@
+
+//Chapter 2, Homework 1
+public class HW2_1 {
+	public static void main(String[] args) {
+		System.out.println("*");
+		System.out.println("* *");		           
+		System.out.println("* * *");				   
+		System.out.println("* * * *");				   
+		System.out.println("* * * * *");				   
+	}
+}
