081c2ec45a5759daae45f0a81c454f12d0a9bcdc
Dhyeya <dhyeya.chandi@gmail.com>
2021-06-24 12:07:38 -0400

    Completed.

--- 3Variables/src/HW3_1.java
+++ 3Variables/src/HW3_1.java
@@ -1,4 +1,14 @@
 
 public class HW3_1 {
-
+	
+	// This is the main method
+	
+	/* Execution of program
+	 * begins with main method
+	 */
+	public static void main(String[] args) {
+		
+	/** This line will be used to print the message */
+		System.out.println("Hello! My name is Dhyeya.");
+	}
 }
