2dc3dfb58d5ecb9038a831e519fd103a7732dda0
Dhyeya <dhyeya.chandi@gmail.com>
2021-06-24 11:15:09 -0400

    Success.

---
+++ 3Variables/src/Comments.java
@@ -0,0 +1,7 @@
+
+public class Comments {
+	public static void main(String[] args) {
+		// This is a sample program
+		System.out.println("This is a sample program"); // Comment on same line without other statement
+	}
+}
---
+++ 3Variables/src/DataTypes.java
@@ -0,0 +1,28 @@
+
+public class DataTypes {
+	public static void main(String[] args) {
+		// int variable with value
+		int speed = 100;
+		long bigNumber;
+		
+		// Assign value to var
+		bigNumber = 22000l;
+		
+		int someIntNumber;
+		
+		// Assigning value from other variable
+		someIntNumber = speed;
+		
+		float PI = 3.14f;
+		
+		String name = "Dhyeya";
+		
+		char firstChar = 'a';
+		
+		boolean greaterNumber = true;
+		
+		System.out.println("Car is running with speed " + speed);
+		
+		System.out.println("My name is " + name.toUpperCase());
+	}
+}
