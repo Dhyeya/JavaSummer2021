54b47d9eb280afc59c0d4a872fc76faffbc7c727
Dhyeya <dhyeya.chandi@gmail.com>
2021-06-27 19:03:34 -0400

    Completed.

---
+++ 3Variables/src/HW3_5.java
@@ -0,0 +1,21 @@
+// HW3_5 (Unary Operation)
+public class HW3_5 {
+	public static void main (String[] arg) {
+		
+		// 1. Define a variables and increment the variable value using unary operation (+)
+		int numOne = 1;
+		numOne++;
+		System.out.println("Value of numOne after increment operation: " + numOne);
+		
+		// 2. Define a variables and decrease the variable value using unary operation (-)					
+		int numTwo = 2;
+		--numTwo;
+		System.out.println("Value of numTwo after increment operation: " + numTwo);
+		
+		// 3. Define a boolean variable. Invert the boolean value and print the value.
+		boolean bFact = true;
+		bFact = !bFact;
+		System.out.println("Value of bFact after inverse operation: " + bFact);
+		
+	}
+}
---
+++ 3Variables/src/HW3_6.java
@@ -0,0 +1,26 @@
+// HW3_6 (Equality Operation)
+public class HW3_6 {
+	public static void main(String[] arg) {
+		
+		// 1. Define a 2 variables and print if first number is greater than second
+		int numOne = 5;
+		int numTwo = 3;
+		
+		if (numOne > numTwo)
+			System.out.println(numOne + " is greater than " + numTwo);
+		
+		// 2. Define a 2 variables and print if first number is less than second
+		int numThree = 2;
+		int numFour = 7;
+		
+		if (numThree < numFour)
+			System.out.println(numThree + " is less than " + numFour);
+		
+		// 3. Define a 2 variables and print if they are equal
+		int numFive = 9;
+		int numSix = 9;
+		
+		if (numFive == numSix)
+			System.out.println(numFive + " is equal to " + numSix);
+	}
+}
