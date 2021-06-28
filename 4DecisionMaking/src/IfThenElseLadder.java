ba1b7602325c10d5fa0a99f631a9b8b7d6bd3954
Dhyeya <dhyeya.chandi@gmail.com>
2021-06-28 18:52:06 -0400

    Done.

---
+++ 4DecisionMaking/src/IfThenElseLadder.java
@@ -0,0 +1,25 @@
+
+public class IfThenElseLadder {
+	public static void main(String[] arg) {
+		int num1 = 3;
+		
+		if (num1 == 1) {
+			System.out.println("Today is Sunday");
+		} else if (num1 == 2 ) {
+			System.out.println("Today is Monday");
+		} else if (num1 == 3) {
+			System.out.println("Today is Tuesday");
+		} else if (num1 == 4) {
+			System.out.println("Today is Wednesday");
+		} else if (num1 == 5) {
+			System.out.println("Today is Thursday");
+		} else if (num1 == 6) {
+			System.out.println("Today is Friday");
+		} else if (num1 == 7) {
+			System.out.println("Today is Saturday");
+		} else {
+			System.out.println("Go see a psychiatrist");
+		}
+		
+	}
+}
---
+++ 4DecisionMaking/src/PrintEvenOdd.java
@@ -0,0 +1,12 @@
+
+public class PrintEvenOdd {
+	public static void main(String[] arg) {
+		int number = 20;
+		
+		if((number %2) == 0) {
+		System.out.println("This number is even");
+		} else {
+		System.out.println("This number is odd");
+		}
+	}
+}
