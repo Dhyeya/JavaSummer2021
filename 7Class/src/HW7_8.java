68e65d6dacc751d21ace9b60974bed5e88f1b728
Dhyeya <dhyeya.chandi@gmail.com>
2021-07-09 15:31:27 -0400

    Completed.

--- 7Class/src/HW7_8.java
+++ 7Class/src/HW7_8.java
@@ -1,11 +1,24 @@
 
 public class HW7_8 {
 	public static void main(String[] args) {
-		int userNum = ReadUserInput.readUserIntegerInput("Enter a number between 25 and 2000");
+		int myNum = 25;
+		
+		for(myNum = 25; myNum <= 2000 ; myNum++) {
+			if(isPrime(myNum) == true) {
+				System.out.println(myNum);
+			}
+		}
 	}
 	
-	public static boolean isPrime(int userNum) {
-		return false;
+	public static boolean isPrime(int checkNum) {
+		int divisCounter = 2;
+		for(divisCounter = 2; divisCounter < checkNum; divisCounter++) {
+			if(checkNum % divisCounter == 0) {
+				return false;
+			} 
+			 
+		}
 		
+		return true;
 	}
 }
--- 7Class/src/Lamp.java
+++ 7Class/src/Lamp.java
@@ -1,17 +1,35 @@
 
 public class Lamp {
-	
-	public String lampSwitch = ReadUserInput.readUserStringInput("Switch on or off:");
-	
-	public void switchOn(boolean lampSwitch) {
-		lampSwitch = true;
+	// switchOn
+	boolean switchOn;
+
+	public void setSwitchOn(boolean lampSwitch, boolean switchOn) {
+		this.switchOn = switchOn;
 	}
 	
-	public void switchOff(boolean lampSwitch) {
-		lampSwitch = false; 
+	public boolean getSwitchOn() {
+		return switchOn;
 	}
 	
-	public void isLampOn(boolean lampSwitch) {
-		lampSwitch = false;
+	// switchOff
+	boolean switchOff;
+	
+	public void setSwitchOff(boolean lampSwitch, boolean switchOff) {
+		this.switchOff = switchOff;
+	}
+	
+	public boolean getSwitchOff(boolean lampSwitch) {
+		return switchOff;
+	}
+	
+	//	isLampOn
+	boolean isLampOn;
+	
+	public void setIsLampOn(boolean lampSwitch, boolean isLampOn) {
+		this.isLampOn = isLampOn;
+	}
+	
+	public boolean getIsLampOn(boolean lampSwitch) {
+		return isLampOn;
 	}
 }
